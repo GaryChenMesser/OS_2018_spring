a0123fa71928759a9a9a10b0365986a668d457f4
GaryChenMesser <gary9192631770@gmail.com>
2018-03-10 21:37:07 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -6,4 +6,18 @@
 #define HELLO_IOCTL_PRINT    0x12345678
 
 static int hello_open(struct inode* inode, struct file* file);
-static
+static int hello_close(struct inode* inode, struct file* file);
+static long hello_ioctl(struct file* file, unsigned int ioctl_num, unsigned long ioctl_param);
+
+static struct file_operations hello_fops = {
+  .owner = THIS_MODULE,
+  .unlocked_ioctl = hello_ioctl,
+  .open = hello_open,
+  .release = hello_close
+};
+
+static struct miscdevice hello_dev = {
+  .minor = MISC_DYNAMIC_MINOR,
+  .name = "hello",
+  .fops = &hello_fops
+};
